9221398f7b064799f65bd367e5e79f28ff16e0fc
Darshini <ddarshini804@gmail.com>
2026-08-25 21:07:58 +0530

    Time: 21 ms (82.09%) | Memory: 21.1 MB (65.00%) - LeetSync

--- 690-employee-importance/employee-importance.cpp
+++ 690-employee-importance/employee-importance.cpp
@@ -14,14 +14,19 @@ public:
         unordered_map<int,Employee*> m;
         for(auto x:employees) m[x->id]=x;
         int sum=0;
-        deque<Employee*>q;
-        q.push_back(m[id]);
-        while(!q.empty()){
-            auto p=q.front();
-            q.pop_front();
-            for(auto x:p->subordinates) q.push_back(m[x]);
-            sum+=p->importance;
-        }
+        // deque<Employee*>q;
+        // q.push_back(m[id]);
+        // while(!q.empty()){
+        //     auto p=q.front();
+        //     q.pop_front();
+        //     for(auto x:p->subordinates) q.push_back(m[x]);
+        //     sum+=p->importance;
+        // }
+        DFS(m,id,sum);
         return sum;
+    }
+    void DFS(unordered_map<int,Employee*>& m,int id,int& sum){
+        sum+=m[id]->importance;
+        for(auto x:m[id]->subordinates) DFS(m,x,sum);
     }
 };
