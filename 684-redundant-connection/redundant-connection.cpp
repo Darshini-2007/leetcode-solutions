b3a127eb50de7e6fd1a9fe95f96d5e2b6a102dfc
Darshini <ddarshini804@gmail.com>
2026-08-27 18:05:19 +0530

    Time: 11 ms (13.70%) | Memory: 14.6 MB (19.33%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool dfs(int s,int parent,vector<int> adj[],vector<bool>& vis){
+        vis[s]=true;
+        for(auto adjnode:adj[s]){
+            if(!vis[adjnode]){
+                if(dfs(adjnode,s,adj,vis))
+                  return true;
+            }
+            else if(adjnode!=parent) return true;
+        }
+        return false;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        vector<int> adj[n+1];
+        for(int i=0;i<n;i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+            vector<bool> vis(n+1,false);
+            if(dfs(edges[i][0],-1,adj,vis)){
+                return edges[i];
+            }
+        }
+        return {};
+    }
+};
